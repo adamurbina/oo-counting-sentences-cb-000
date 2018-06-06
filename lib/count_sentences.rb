b5f02176c79812e6c6d31d0c38222b7843136f27
Adam Urbina <adamurbina@gmail.com>
2018-06-06 14:02:59 +0000

    automatically backed up by learn

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -7,14 +7,15 @@ class String
   end
 
   def question?
-
+      self.end_with?("?") ? true : false
   end
 
   def exclamation?
-
+      self.end_with?("!") : true : false
   end
 
   def count_sentences
-
+      sentences = self.split(/[!?.]/)
+      sentences.count{ |text| text.length > 0}
   end
 end
